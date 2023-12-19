e05e5a7585124f6ce7752ef122b39eeec600d8bc
Rajkumar Dongre <rajkumardongre17@gmail.com>
2023-12-20 05:15:28 +0530

    fix

--- LadleVision/web/db/ladle.js
+++ LadleVision/web/db/ladle.js
@@ -69,37 +69,19 @@ export async function getHaltLadle(thresholdSeconds) {
     const currentTime = new Date();
 
     const [rows] = await pool.query(
-<<<<<<< HEAD:LadleVision/web/db/ladle.js
       "SELECT * FROM ladle WHERE Location NOT IN (1, 9)"
-=======
-      `SELECT * FROM ladle`
->>>>>>> 15a4617cc59fd4847a3bd6f60d61e95b632798a4:web/db/ladle.js
     );
     // Filter rows based on the threshold
     const filteredRows = rows.filter((row) => {
       const lastUpdatedTime = moment(row.LastUpdated);
-<<<<<<< HEAD:LadleVision/web/db/ladle.js
-//      console.log("lastUpdatedTime:", lastUpdatedTime)
-    console.log(row.LastUpdated)
-=======
->>>>>>> 15a4617cc59fd4847a3bd6f60d61e95b632798a4:web/db/ladle.js
       const timeDifference = moment
         .duration(currentTime - lastUpdatedTime)
         .asSeconds();
       return timeDifference > thresholdSeconds;
-    });
-<<<<<<< HEAD:LadleVision/web/db/ladle.js
-    console.log("rows:",filteredRows)
-=======
-
->>>>>>> 15a4617cc59fd4847a3bd6f60d61e95b632798a4:web/db/ladle.js
-    return filteredRows;
+    });    return filteredRows;
   } catch (error) {
     console.error("Error getting halted ladles:", error);
     throw error;
-<<<<<<< HEAD:LadleVision/web/db/ladle.js
-=======
+
   }
->>>>>>> 15a4617cc59fd4847a3bd6f60d61e95b632798a4:web/db/ladle.js
-}
 }
